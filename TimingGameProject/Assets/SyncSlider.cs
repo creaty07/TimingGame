79d5c866019e14173e0c26405aecba4578b189a3
creaty07 <creaty0722@gmail.com>
2023-04-03 22:27:48 +0900

    First Project Commit
    First VRC World Commit

---
+++ TimingGameProject/Assets/SyncSlider.cs
@@ -0,0 +1,48 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class SyncSlider : UdonSharpBehaviour
+{
+    [UdonSynced]
+    private float syncedValue;
+    private float localValue;
+    private bool deserializing;
+    private VRCPlayerApi localPlayer;
+    private Slider slider;
+
+    void Start()
+    {
+        slider = transform.GetComponent<Slider>();
+        localPlayer = Networking.LocalPlayer;
+        syncedValue = localValue = slider.value;
+        deserializing = false;
+    }
+
+    public override void OnPreSerialization()
+    {
+        syncedValue = localValue;
+    }
+
+    public override void OnDeserialization()
+    {
+        deserializing = true;
+
+        if (!Networking.IsOwner(gameObject))
+        {
+            slider.value = syncedValue;
+        }
+
+        deserializing = false;  
+    }
+
+    public void SliderUpdate()
+    {
+        if (!Networking.IsOwner(gameObject) && !deserializing) Networking.SetOwner(localPlayer, gameObject);
+
+        localValue = syncedValue = slider.value;
+    }
+}
