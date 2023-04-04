f537dd8a6541a0ecdddea95b6edd09e65dba8e2f
creaty07 <creaty0722@gmail.com>
2023-04-04 23:39:06 +0900

    게임 룰 작업 진행 중
    게임 시작할 때 데이터 초기화 작업 진행 중

---
+++ TimingGameProject/Assets/GameManager.cs
@@ -0,0 +1,31 @@
+﻿
+using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class GameManager : UdonSharpBehaviour
+{
+    [UdonSynced]
+    private byte maxRound; // 최대 라운드
+    [UdonSynced]
+    private byte round; // 게임 진행 라운드
+    [UdonSynced]
+    private byte[] numbers; // 내 숫자 리스트
+    [UdonSynced]
+    private GameState gameState; // 게임 진행 상태
+
+    public GameRule rule;
+
+    void Start()
+    {
+
+    }
+  
+
+}
